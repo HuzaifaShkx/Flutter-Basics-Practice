09f23e9e6be2bb949501e0e5ea24c133949bf7c2
HuzaifaShkx <huzaifayasin225@gmail.com>
2022-07-07 22:48:56 +0500

    chap#2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,102 @@ class homepage extends StatelessWidget {
           backgroundColor: Colors.green,
           title: Text("Awesome App"),
       ),
-      body: Container(
-         child: Center(child: Text("Welcome to my app")),
-         
-
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0), 
+        child: Align(
+          alignment: Alignment.center,
+                  child: Container(
+            width:200,
+            height: 300,
+            color: Colors.teal,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+                   children:[
+                      Container(
+                  padding: EdgeInsets.all(20.0) ,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10,
+                      )
+                    ],
+                    color: Colors.teal,
+                    gradient: LinearGradient(colors:[Colors.lime, Colors.green,Colors.amber])
+                  ),
+                  width: 100,
+                  height: 100,
+                  child: Text("I am a Box",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                  ),
+                ) ,
+                 Container(
+                  padding: EdgeInsets.all(20.0) ,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10,
+                      )
+                    ],
+                    color: Colors.teal,
+                    gradient: LinearGradient(colors:[Colors.lime, Colors.green,Colors.amber])
+                  ),
+                  width: 100,
+                  height: 100,
+                  child: Text("I am a Box",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                  ),
+                ) ,
+                 Container(
+                  padding: EdgeInsets.all(20.0) ,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10,
+                      )
+                    ],
+                    color: Colors.teal,
+                    gradient: LinearGradient(colors:[Colors.lime, Colors.green,Colors.amber])
+                  ),
+                  width: 100,
+                  height: 100,
+                  child: Text("I am a Box",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                  ),
+                ) ,
+                   ]
+                  
+              ),
+          ),
+        ),
+      )
     );
   }
 }
