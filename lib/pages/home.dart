0fba1c6bbb4b6e75902acaa8392af3aff7f9e0c0
HuzaifaShkx <huzaifayasin225@gmail.com>
2022-07-09 22:38:41 +0500

    chap # 4 completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
-import '../bgimage.dart';
+import "package:http/http.dart" as http;
 import '../drawer.dart';
-
+import 'dart:convert';
 class homepage extends StatefulWidget {
   @override
   State<homepage> createState() => _homepageState();
@@ -12,11 +11,24 @@ class _homepageState extends State<homepage> {
 
   TextEditingController _namecontroller=new TextEditingController();
   var mytext="Change Me";
+  var abc="https://http://jsonplaceholder.typicode.com/photos";
+  var data;
+
   @override
   void initState() {
       // TODO: implement initState
       super.initState();
+      getData();
     }
+    getData() async {
+      var res = await http.get(abc);
+      print(res.body);
+      data=jsonDecode(res.body);
+      setState(() {
+              
+            });
+    }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
@@ -25,37 +37,40 @@ class _homepageState extends State<homepage> {
       ),
       body:Padding(
         padding: const EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
-            child: Card(
-            child: Column(
-              children: [
-               bgimage(),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                mytext,
-                style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
-                    controller: _namecontroller,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      hintText: "Enter something",
-                      labelText: "Name",
-                      border: OutlineInputBorder(),
+       
+            child:data!=null?
+             SingleChildScrollView(
+                          child: Card(
+           /* child: Column(
+                children: [
+                 bgimage(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                  mytext,
+                  style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: _namecontroller,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        hintText: "Enter something",
+                        labelText: "Name",
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 50,),
-                ],
-                
-            ),
+                  SizedBox(height: 50,),
+                  ],
+                  
+              ),*/
           ),
-        ),
+            ):Center(child: CircularProgressIndicator(),)
+      
       ),
       drawer: myDrawer(),
       floatingActionButton: FloatingActionButton( 
